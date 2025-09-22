cc7a5a0e6d52a83ea2bbbc1fbdbde02bddcc3fba
LilianeFalcao <lilianefalcao2293@gmail.com>
2025-09-22 18:08:32 -0300

    Terminando o 'User.ts'

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,14 +1,19 @@
 import { Email } from "../value-objects/Email";
 import { Senha } from "../value-objects/Senha";
 
-export class Users{
+export class User{
     private constructor(
         readonly id: string,
         readonly nickname: string,
         readonly email: Email,
         readonly senha: Senha
-    ){
-
+    ){  }
+    static create(
+        id: string,
+        nickname: string,
+        email: Email,
+        senha: Senha
+    ): User {
+        return new User(id, nickname, email, senha)
     }
-
 }
